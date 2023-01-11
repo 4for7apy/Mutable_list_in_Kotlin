c850e381c1ac1929fea798c121799ba2b9797c3c
arpit <arpit88407@gmail.com>
2023-01-11 21:30:42 +0530

    Initial commit

---
+++ src/main/kotlin/hii.kt
@@ -0,0 +1,6 @@
+fun main(){
+    val nameofStudents = mutableListOf("Arpit","Ansh","Ans","Anupma","Anil","Arpita")
+    for (name in 1..5){
+        println("$name : ${nameofStudents[name]}")
+    }
+}
